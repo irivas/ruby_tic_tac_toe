2f8851aea2ff799d271267fff6f1ab66b309720c
Matt Glover <matthewglover@me.com>
2017-09-07 18:57:07 +0100

    Update tic_tac_toe app script to use GameRunner

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,20 +1,4 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__) + "/lib")
-require "game_builder"
-require "human_player"
-require "computer_player"
-require "player_ui_builder"
-require "game_result_ui_builder"
-require "board"
-require "mini_max"
+require "game_runner"
 
-player_x = HumanPlayer.new(PlayerUIBuilder.new)
-player_o = ComputerPlayer.new(PlayerUIBuilder.new, MiniMax, sleep_time: 1, max_depth: 4)
-
-GameBuilder
-  .new
-  .add_player_x(player_x)
-  .add_player_o(player_o)
-  .add_board(Board.new)
-  .add_result_ui_builder(GameResultUIBuilder.new)
-  .build
-  .run
+GameRunner.new.run
