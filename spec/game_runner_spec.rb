235516401f3d08f4fe0260ace5eb5ceb8e7566bf
Matt Glover <matthewglover@me.com>
2017-09-07 18:56:40 +0100

    Add GameRunner

---
+++ spec/game_runner_spec.rb
@@ -0,0 +1,16 @@
+require "game_runner"
+
+describe GameRunner do
+  describe "#run" do
+    it "runs a game" do
+      instructions = "1\n1\n4\n2\n5\n3\nn\n"
+      game_runner(instructions).run
+      expect(@output.string).to match(/X wins!/)
+    end
+
+    def game_runner(input)
+      @output = StringIO.new
+      GameRunner.new(input: StringIO.new(input), output: @output)
+    end
+  end
+end
