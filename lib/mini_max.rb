c7c6d74df7d2e107963aab15577cbd397f0102b7
Matt Glover <matthewglover@me.com>
2017-09-11 10:27:03 +0100

    Implement alpha beta algorithm in AlphaBeta
    - Move execute from MiniMaxable to MiniMax
    - Implement execute in AlphaBeta

--- lib/mini_max.rb
+++ lib/mini_max.rb
@@ -1,4 +1,8 @@
 require "mini_maxable"
 
 class MiniMax < MiniMaxable
+  def execute
+    @move, @score = move_scores.send(max_or_min_by, &:last)
+    self
+  end
 end
--- lib/mini_maxable.rb
+++ lib/mini_maxable.rb
@@ -14,8 +14,7 @@ class MiniMaxable
   end
 
   def execute
-    @move, @score = move_scores.send(max_or_min_by, &:last)
-    self
+    raise "Method not implemented"
   end
 
   private
