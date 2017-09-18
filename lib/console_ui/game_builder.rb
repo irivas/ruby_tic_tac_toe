10b38161aa03f998a6f21509b0a2a97d7909440e
Matt Glover <matthewglover@me.com>
2017-09-18 10:33:59 +0100

    Move Game and GameRunner from Core to ConsoleUI

---
+++ lib/console_ui/game_builder.rb
@@ -0,0 +1,35 @@
+require "console_ui/game"
+
+module ConsoleUI
+  class GameBuilder
+    def initialize
+      @args = Hash.new
+    end
+
+    def add_player_x(player)
+      set_arg(:player_x, player)
+    end
+
+    def add_player_o(player)
+      set_arg(:player_o, player)
+    end
+
+    def add_board(board)
+      set_arg(:board, board)
+    end
+
+    def add_result_ui_builder(result_ui_builder)
+      set_arg(:result_ui_builder, result_ui_builder)
+    end
+
+    def build
+      ConsoleUI::Game.new(@args)
+    end
+
+    private
+    def set_arg(key, value)
+      @args[key] = value
+      self
+    end
+  end
+end
--- spec/console_ui/game_spec.rb
+++ spec/console_ui/game_spec.rb
@@ -1,18 +1,18 @@
-require "core/game_builder"
-require "core/game"
+require "console_ui/game_builder"
+require "console_ui/game"
 require "core/board"
 require "core/human_player"
 require "console_ui/player_ui_builder"
 require "console_ui/game_result_ui_builder"
 
-describe Game do
+describe ConsoleUI::Game do
   describe "#run" do
     it "plays game until complete" do
       player_x = build_player("1\n2\n3\n")
       player_o = build_player("4\n5\n")
       formatted_final_board = ConsoleUI::BoardFormatter.new(build_board(1, 4, 2, 5, 3)).format
 
-      game = GameBuilder
+      game = ConsoleUI::GameBuilder
         .new
         .add_player_x(player_x)
         .add_player_o(player_o)
